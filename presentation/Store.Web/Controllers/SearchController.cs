d34b39da1df8d3597d6fac4b86a9cbc63992ef68
deni_saintp <denisdmitriev6@gmail.com>
2022-01-16 02:23:31 +0300

    Добавить страницу поиска

---
+++ presentation/Store.Web/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Web.Controllers
+{
+	public class SearchController : Controller
+	{
+		private readonly IBookRepository bookRepository; //Констрактор-Инжекшн
+
+		public SearchController(IBookRepository bookRepository) //Констрактор-Инжекшн
+		{
+			this.bookRepository = bookRepository;
+		}
+		public IActionResult Index(string query)
+		{
+			var books = bookRepository.GetAllByTitle(query);
+			return View(books);
+		}
+	}
+}
