8c2835c23a1899fdc3755e6762d0beed460bf8f1
deni_saintp <denisdmitriev6@gmail.com>
2022-01-16 00:47:21 +0300

    Описать сущность Book

---
+++ domain/Store/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Store
+{
+    public class Book
+    {
+        public int Id { get; }
+        public string title;
+        public string Title { get; } //Свойство только на чтение. Посмотреть, как делал свойства ITDVN
+
+        public Book(int id,string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+    }
+}
