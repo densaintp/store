1bad92ccabf4003c09f2e2beb850e91a6d4f5444
deni_saintp <denisdmitriev6@gmail.com>
2022-01-17 17:19:23 +0300

    Реализовать поиск по ISBN и автору

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -7,12 +7,25 @@ namespace Store.Memory
 	{
 		private readonly Book[] books = new[]
 		{
-		new Book(1, "Art of Programming"),
-		new Book(2, "Refactoring"),
-		new Book(3, "C Programming Language"),
+		new Book(1, "ISBN 12312-31231","D. Knuth", "Art of Programming"),
+		new Book(2, "ISBN 12312-31232","M. Fowler","Refactoring"),
+		new Book(3, "ISBN 12312-31233", "B. Kernighan, D. Ritchie", "C Programming Language"),
 		};
 
-		public Book[] GetAllByTitle(string titlePart)
+		public Book[] GetAllByIsbn(string isbn) //Linq запрос
+		{
+			return books.Where(book => book.Isbn == isbn)
+				        .ToArray();
+		}
+
+		public Book[] GetAllByTitleOrAuthor(string query)
+		{
+			return books.Where(book => book.Author.Contains(query) //Лямбда-выражение
+				                    ||book.Title.Contains(query))
+						.ToArray();
+		}
+
+		public Book[] GetAllByTitle(string titlePart) //Возможно это надо удалить??
 		{
 			return books.Where(book => book.Title.Contains(titlePart)) //Лямбда-выражение
 						.ToArray();
