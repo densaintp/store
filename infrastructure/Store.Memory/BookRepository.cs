f6079960e96ca92b8ba3fbf4fafd90f297b12767
deni_saintp <denisdmitriev6@gmail.com>
2022-01-30 22:37:02 +0300

    Добавить страницу описания книги

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -7,9 +7,9 @@ namespace Store.Memory
 	{
 		private readonly Book[] books = new[]
 		{
-		new Book(1, "ISBN 12312-31231","D. Knuth", "Art of Programming"),
-		new Book(2, "ISBN 12312-31232","M. Fowler","Refactoring"),
-		new Book(3, "ISBN 12312-31233", "B. Kernighan, D. Ritchie", "C Programming Language"),
+		new Book(1, "ISBN 12312-31231","D. Knuth", "Art of Programming", "описание", 12.21m),
+		new Book(2, "ISBN 12312-31232","M. Fowler","Refactoring", "описание", 11.21m),
+		new Book(3, "ISBN 12312-31233", "B. Kernighan, D. Ritchie", "C Programming Language", "описание", 7.21m),
 		};
 
 		public Book[] GetAllByIsbn(string isbn) //Linq запрос
@@ -29,6 +29,11 @@ namespace Store.Memory
 		{
 			return books.Where(book => book.Title.Contains(titlePart)) //Лямбда-выражение
 						.ToArray();
+		}
+
+		public Book GetById(int id)
+		{
+			return books.Single(book => book.Id == id); //LINQ
 		}
 	}
 }
