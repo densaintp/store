29d3aad28fada040addda3395ad214a4716f4ee3
deni_saintp <denisdmitriev6@gmail.com>
2022-02-04 03:24:05 +0300

    Исправил ошибки, товары в корзине теперь списком

--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -1,60 +1,81 @@
-﻿using Store.Web.Models;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
-using System.Linq;
+using Store.Web.Models;
 
 namespace Store.Web.Controllers
 {
-	public class OrderController : Controller
-	{
-		private readonly IBookRepository bookRepository;
-		private readonly IOrderRepository orderRepository;
+    public class OrderController : Controller
+    {
+        private readonly IBookRepository bookRepository;
+        private readonly IOrderRepository orderRepository;
 
-		public OrderController(IBookRepository bookRepository,
-							  IOrderRepository orderRepository)
-		{
-			this.bookRepository = bookRepository;
-			this.orderRepository = orderRepository;
-		}
+        public OrderController(IBookRepository bookRepository,
+                              IOrderRepository orderRepository)
+        {
+            this.bookRepository = bookRepository;
+            this.orderRepository = orderRepository;
+        }
 
-		public IActionResult Index()
-		{
-			if (HttpContext.Session.TryGetCart(out Cart cart))
-			{
-				var order = orderRepository.GetById(cart.OrderId);
-				OrderModel model = Map(order);
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.TryGetCart(out Cart cart))
+            {
+                var order = orderRepository.GetById(cart.OrderId);
+                OrderModel model = Map(order);
 
-				return View(model);
-			}
+                return View(model);
+            }
 
-			return View("Empty");
-		}
-		public IActionResult AddItem(int id)
-		{
+            return View("Empty");
+        }
 
-			Order order;
-			Cart cart;
-			if (HttpContext.Session.TryGetCart(out cart))
-			{
-				order = orderRepository.GetById(cart.OrderId);
-			}
-			else
-			{
-				order = orderRepository.Create();
-				cart = new Cart(order.Id);
-			}
+        private OrderModel Map(Order order)
+        {
+            var bookIds = order.Items.Select(item => item.BookId);
+            var books = bookRepository.GetAllByIds(bookIds);
+            var itemModels = from item in order.Items
+                             join book in books on item.BookId equals book.Id
+                             select new OrderItemModel
+                             {
+                                 BookId = book.Id,
+                                 Title = book.Title,
+                                 Author = book.Author,
+                                 Price = item.Price,
+                                 Count = item.Count,
+                             };
 
-			var book = bookRepository.GetById(id);
-			order.AddItem(book, 1);
-			orderRepository.Update(order);
+            return new OrderModel
+            {
+                Id = order.Id,
+                Items = itemModels.ToArray(),
+                TotalCount = order.TotalCount,
+                TotalPrice = order.TotalPrice,
+            };
+        }
 
-			cart.TotalCount = order.TotalCount;
-			cart.TotalPrice = order.TotalPrice;
-			HttpContext.Session.Set(cart);
+        public IActionResult AddItem(int id)
+        {
+            Order order;
+            Cart cart;
+            if (HttpContext.Session.TryGetCart(out cart))
+            {
+                order = orderRepository.GetById(cart.OrderId);
+            }
+            else
+            {
+                order = orderRepository.Create();
+                cart = new Cart(order.Id);
+            }
 
-			return RedirectToAction("Index", "Book", new { id });
+            var book = bookRepository.GetById(id);
+            order.AddItem(book, 1);
+            orderRepository.Update(order);
 
-			}
-		}
-	}
+            cart.TotalCount = order.TotalCount;
+            cart.TotalPrice = order.TotalPrice;
+            HttpContext.Session.Set(cart);
 
+            return RedirectToAction("Index", "Book", new { id });
+        }
+    }
+}
